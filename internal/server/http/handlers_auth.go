e3d5fbeefa3735d5a2e673ad7ca19e2d0925293c
J.P. Cummins <jp@sat.watch>
2025-05-02 11:11:23 -0700

    Fixed login redirect

--- internal/server/http/handlers_auth.go
+++ internal/server/http/handlers_auth.go
@@ -3,6 +3,7 @@ package http
 import (
 	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/jpcummins/satwatch/internal/api"
 	"github.com/jpcummins/satwatch/internal/configs"
@@ -56,6 +57,11 @@ func (ac AuthController) Login(c echo.Context) error {
 	if err := sess.Save(c.Request(), c.Response()); err != nil {
 		logger(c).Err(err).Msg("unable to save session")
 		return Render(c, http.StatusInternalServerError, templates.PageLogin(errors.New("An unexpected error occurred")))
+	}
+
+	redirect := c.QueryParam("redirect")
+	if strings.HasPrefix(redirect, "/app/settings/email/") {
+		return c.Redirect(http.StatusSeeOther, redirect)
 	}
 
 	return c.Redirect(http.StatusSeeOther, "/app")
