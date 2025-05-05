8fe008f32ce738d427b5ad4d3d45c2c894167668
J.P. Cummins <jp@sat.watch>
2025-05-05 12:03:42 -0700

    Fixed race condition when adding new addresses

--- internal/api/address.go
+++ internal/api/address.go
@@ -40,8 +40,6 @@ func (api *API) CreateAddress(addresses ...Address) error {
 			address.Name,
 			address.IsExternal,
 			address.AddressIndex)
-
-		api.monitor.EnqueueScan(scripthash)
 	}
 
 	err := api.db.SendBatch(context.Background(), batch).Close()
@@ -52,6 +50,12 @@ func (api *API) CreateAddress(addresses ...Address) error {
 	api.mu.Lock()
 	defer api.mu.Unlock()
 	api.addresses = append(api.addresses, addresses...)
+
+	// enqueue UTXO scans now that addresses are in-memory
+	for _, addr := range addresses {
+		api.monitor.EnqueueScan(addr.Scripthash)
+	}
+
 	return nil
 }
 
